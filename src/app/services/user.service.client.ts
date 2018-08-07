50f3dd87a76f747ebcef4f8154584d80c701891e
rashichauhan7 <rashi.chauhan95@gmail.com>
2018-08-07 18:29:08 -0400

    register username check

--- src/app/services/user.service.client.ts
+++ src/app/services/user.service.client.ts
@@ -3,7 +3,7 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class UserServiceClient {
   findUserByUserName = (user) =>
-    fetch('http://localhost:4000/api/user', {
+    fetch('https://webdev-angular-node-server.herokuapp.com/api/user', {
       method: 'post',
       credentials: 'include',
       headers: {
